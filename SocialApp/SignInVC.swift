00270e291e596e2302b08c05080067c2520e1649
Jeff Ripke <jripke74@gmail.com>
2017-06-06 10:49:35 -0700

    add mvc folders and changed ViewController name

---
+++ SocialApp/SignInVC.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Jeff Ripke on 6/6/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+    
+}
