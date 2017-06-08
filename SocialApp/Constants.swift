0771a61acbf12e96098c8229ce219e95d48198ff
Jeff Ripke <jripke74@gmail.com>
2017-06-08 07:10:28 -0700

    sign in ui custom classes

---
+++ SocialApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialApp
+//
+//  Created by Jeff Ripke on 6/7/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
