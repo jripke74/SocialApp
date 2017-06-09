c6f8ed4a00a06afc91ddc5a1d82cabfee7cdbabc
Jeff Ripke <jripke74@gmail.com>
2017-06-09 09:07:50 -0700

    add facebook auth

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -7,7 +7,35 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
+import FirebaseAuth
 
 class SignInVC: UIViewController {
     
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("JEFF: Unable to authenticate with Facebook - \(error!)")
+            } else if result?.isCancelled == true {
+                print("JEFF: User cancelled Facebook authentication")
+            } else {
+                print("JEFF: Successfully authenticate with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("JEFF: Unable to authenticate with Firebase - \(error!)")
+            } else {
+                print("JEFF: Successfully authenticated with Firebase")
+            }
+        })
+    }
 }
