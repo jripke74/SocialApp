bbab292960682ae5fb0f3eab99cfab547744ac1b
Jeff Ripke <jripke74@gmail.com>
2017-06-11 08:13:00 -0700

    add feed

---
+++ SocialApp/PostCell.swift
@@ -0,0 +1,20 @@
+//
+//  PostCell.swift
+//  SocialApp
+//
+//  Created by Jeff Ripke on 6/11/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    
+}
