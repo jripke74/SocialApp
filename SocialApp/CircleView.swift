aac35e7fc5a3a10b5717c66f70ac2ce1314b47bd
Jeff Ripke <jripke74@gmail.com>
2017-06-11 06:24:57 -0700

    add feed-ui

--- SocialApp/CircleView.swift
+++ SocialApp/CircleView.swift
@@ -1,14 +1,14 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  SocialApp
 //
-//  Created by Jeff Ripke on 6/7/17.
+//  Created by Jeff Ripke on 6/11/17.
 //  Copyright © 2017 Jeff Ripke. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -16,6 +16,10 @@ class FancyView: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
     }
 }
