4284d81a62a344755ab9fa2a582dc8c4f19dec22
Jeff Ripke <jripke74@gmail.com>
2017-06-12 12:32:04 -0700

    data parsed

---
+++ SocialApp/Post.swift
@@ -0,0 +1,51 @@
+//
+//  Post.swift
+//  SocialApp
+//
+//  Created by Jeff Ripke on 6/12/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    private var _caption: String!
+    private var _imageURL: String!
+    private var _likes: Int!
+    private var _postKey: String!
+    
+    var caption: String {
+        return _caption
+    }
+    
+    var imageURL: String {
+        return _imageURL
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var postKey: String {
+        return _postKey
+    }
+    
+    init(caption: String, imageURL: String, likes: Int) {
+        self._caption = caption
+        self._imageURL = imageURL
+        self._likes = likes
+    }
+    
+    init(postKey: String, postData: Dictionary<String, AnyObject>) {
+        self._postKey = postKey
+        if let caption = postData["caption"] as? String {
+            self._caption = caption
+        }
+        if let imageUrl = postData["imageURL"] as? String {
+            self._imageURL = imageUrl
+        }
+        if let likes = postData["likes"] as? Int {
+            self._likes = likes
+        }
+    }
+}
